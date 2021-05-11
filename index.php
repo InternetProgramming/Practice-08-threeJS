1394e0c53f77c7565789d6aa3ae60e95d3dbcf3e
Andrea Rojas <andrear.2810@gmail.com>
2021-05-11 18:06:16 -0500

    change extension from html->php to publish in heroku

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8">
   <title>The Aviator: Claudia Andrea Rojas Márquez</title>
   <link rel="stylesheet" type="text/css" href="css/styles.css" />
-  <script type="text/javascript" src="js/three.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/87/three.js"></script>
   <script type="text/javascript" src="js/index.js"/></script>
 </head>
 <body>
